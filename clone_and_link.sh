f2259bb0d5ee364959d7979ee7f8f10283978b2b
michael <michael@md123058-3.local>
2011-12-19 15:50:29 +1100

    Modified clone script

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -2,7 +2,7 @@
 
 cd &&
 [ -d '.dot-files' ] || git clone https://heathcakes@github.com/heathcakes/dot-files.git .dot-files &&
-ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
+ls -1d .dot-files/files .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
   [ "$f" == '.dot-files/files/.git' ] ||
